8550840d6e6839aac9ff7df6d2a7fb30df125a0c
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-07 15:07:24 +0500

    Update callbacks.py

--- app/keyboards/callbacks.py
+++ app/keyboards/callbacks.py
@@ -1,5 +1,8 @@
 from aiogram.filters.callback_data import CallbackData
 
 
-class LanguageCallback(CallbackData, prefix="lang"):
+class LanguageCallback(
+    CallbackData,
+    prefix="lang",
+):
     language: str
