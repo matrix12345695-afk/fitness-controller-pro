023cfd17010f29a549b203bb8d2f9866b2f054dd
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-07 15:08:37 +0500

    Update profiles.py

--- app/repositories/profiles.py
+++ app/repositories/profiles.py
@@ -4,7 +4,12 @@ from app.models.profile import Profile
 from app.repositories.base import BaseRepository
 
 
-class ProfileRepository(BaseRepository):
+class ProfileRepository(
+    BaseRepository[Profile]
+):
+    """
+    Repository for Profile model.
+    """
 
     async def get(
         self,
@@ -19,9 +24,33 @@ class ProfileRepository(BaseRepository):
 
     async def create(
         self,
-        **kwargs,
+        user_id: int,
+        full_name: str,
+        gender,
+        birth_date,
+        height: int,
+        start_weight: float,
     ) -> Profile:
 
-        profile = Profile(**kwargs)
+        profile = Profile(
+            user_id=user_id,
+            full_name=full_name,
+            gender=gender,
+            birth_date=birth_date,
+            height=height,
+            start_weight=start_weight,
+        )
 
         return await self.add(profile)
+
+    async def update_weight(
+        self,
+        profile: Profile,
+        weight: float,
+    ) -> Profile:
+
+        profile.start_weight = weight
+
+        await self.commit()
+
+        return profile
