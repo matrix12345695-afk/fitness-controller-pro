bd736657d6ebcd60d9e22478aa76431f83b01877
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-07 14:33:04 +0500

    Update base.py

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,1 +1,37 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+
+class BaseRepository:
+    """
+    Base repository.
+    """
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def add(self, instance):
+        self.session.add(instance)
+        await self.session.flush()
+        await self.session.refresh(instance)
+        return instance
+
+    async def delete(self, instance):
+        await self.session.delete(instance)
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
+
+    async def execute(self, statement):
+        return await self.session.execute(statement)
+
+    async def scalar(self, statement):
+        result = await self.session.execute(statement)
+        return result.scalar_one_or_none()
+
+    async def scalars(self, statement):
+        result = await self.session.execute(statement)
+        return result.scalars().all()
