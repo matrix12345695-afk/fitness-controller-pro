1baa51a7e43bc563ce4ae59f43f0b50e2bc375d2
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-07 15:06:42 +0500

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -8,3 +8,14 @@ class UserCreate(BaseModel):
     last_name: str | None = None
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserRead(BaseModel):
+    id: int
+    telegram_id: int
+    username: str | None = None
+    first_name: str
+    last_name: str | None = None
+    language: str
+
+    model_config = ConfigDict(from_attributes=True)
