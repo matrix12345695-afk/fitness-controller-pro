9e2027af51c23bfc037ae2a9a791bf12785e54ad
matrix12345695-afk <matrix12345695@gmail.com>
2026-07-07 14:18:31 +0500

    Update answer.py

---
+++ app/models/answer.py
@@ -0,0 +1,35 @@
+from sqlalchemy import ForeignKey, Text
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import BaseModel
+
+
+class Answer(BaseModel):
+    """
+    Answer for survey question.
+    """
+
+    __tablename__ = "answers"
+
+    report_id: Mapped[int] = mapped_column(
+        ForeignKey("reports.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    question_id: Mapped[int] = mapped_column(
+        ForeignKey("questions.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    answer: Mapped[str] = mapped_column(
+        Text,
+        nullable=False,
+    )
+
+    report = relationship(
+        "Report",
+        back_populates="answers",
+    )
+
+    question = relationship("Question")
